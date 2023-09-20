bf6f79afb20f09702543b522a618676972c6b191
ButchanCrawford <butchan@outlook.com>
2023-09-20 12:29:49 -0700

    V2

--- app.py
+++ app.py
@@ -8,10 +8,21 @@ app = Flask(__name__)
 def index():
     return render_template("index.html", launches = launches) 
 
+@app.route("/launchdetails")
+def individual_launch():
+    print("launch")
+    return render_template("individual_launch.html",launches = launches)
+
+@app.route("/launchdetails", methods=['POST'])
+def  load_individual_launch():
+    print("launch")
+    return render_template("individual_launch.html",launches = launches)
+
 @app.template_filter("date_only")
 def date_only_filter(date_string):
     date = datetime.strptime(date_string,"%Y-%m-%dT%H:%M:%S.%fZ" )
     return date.date()
+
 
 def fetch_spacex_launches():
     url = "https://api.spacexdata.com/v5/launches"
@@ -22,8 +33,10 @@ def fetch_spacex_launches():
         return []
 
 def categorize_launches(Launches):
-    successful = list(filter(lambda x: x["success"] and not x["upcoming"],Launches))
-    failed = list(filter(lambda x: not x["success"] and not x["upcoming"],Launches))
+    successful_list = list(filter(lambda x: x["success"] and not x["upcoming"],Launches))
+    successful = successful_list[::-1]
+    failed_list = list(filter(lambda x: not x["success"] and not x["upcoming"],Launches))
+    failed = failed_list[::-1]
     upcoming = list(filter(lambda x:  x["upcoming"],Launches))
     # successful = [launch for launch in launches launch["success"] and not launch["upcoming"]]
 
@@ -32,6 +45,7 @@ def categorize_launches(Launches):
     
     
 launches = categorize_launches(fetch_spacex_launches())
+
 
 
 
